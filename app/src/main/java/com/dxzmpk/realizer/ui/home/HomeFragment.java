5066e1597adbfd490528edc8801288e04d0a31a5
dxzmpk <1635338905@qq.com>
2021-04-28 16:22:30 +0800

    change context to weak reference to avoid memory leak

--- app/src/main/java/com/dxzmpk/realizer/ui/home/HomeFragment.java
+++ app/src/main/java/com/dxzmpk/realizer/ui/home/HomeFragment.java
@@ -1,43 +1,26 @@
 package com.dxzmpk.realizer.ui.home;
 
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.dxzmpk.realizer.MainActivity;
-import com.dxzmpk.realizer.R;
 import com.dxzmpk.realizer.databinding.FragmentHomeBinding;
-import com.dxzmpk.realizer.ui.receiver.AlarmReceiverService;
 import com.dxzmpk.realizer.ui.receiver.ScreenListener;
 
-import java.lang.ref.WeakReference;
-import java.time.Duration;
 import java.util.Random;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
@@ -47,7 +30,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     private static boolean closed;
 
     private static HomeViewModel homeViewModel;
-    private AlarmManager alarmManager;
     private static NotificationSender notificationSender;
     protected static FragmentHomeBinding binding;
     static int NOTIFY_TIMES = 30;
--- app/src/main/java/com/dxzmpk/realizer/ui/home/NotificationSender.java
+++ app/src/main/java/com/dxzmpk/realizer/ui/home/NotificationSender.java
@@ -14,6 +14,7 @@ import androidx.core.app.NotificationCompat;
 
 import com.dxzmpk.realizer.R;
 
+import java.lang.ref.WeakReference;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -23,15 +24,15 @@ public class NotificationSender {
 
     String channelId = "com.dxzmpk.notify";
     public NotificationManager notificationManager;
-    private Context context;
+    private WeakReference<Context> contextWeakReference;
 
     public NotificationSender(Context context) {
-        this.context = context;
+        this.contextWeakReference = new WeakReference<>(context);
         createManagerChannel();
     }
 
     private void createManagerChannel() {
-        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) contextWeakReference.get().getSystemService(Context.NOTIFICATION_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(channelId, "使用时长通知", NotificationManager.IMPORTANCE_HIGH);
             channel.setVibrationPattern(new long[] { 0, 1000, 1000, 1000, 1000 });
@@ -41,12 +42,12 @@ public class NotificationSender {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void showNotification(int randomId, String text) {
-        Notification notification = new NotificationCompat.Builder(context, channelId)
+        Notification notification = new NotificationCompat.Builder(contextWeakReference.get(), channelId)
                 .setContentTitle("Realizer")
                 .setContentText(text)
                 .setVibrate(new long[] { 0, 1000, 1000, 1000, 1000 })
                 .setSmallIcon(R.drawable.ic_menu_send)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_menu_camera))
+                .setLargeIcon(BitmapFactory.decodeResource(contextWeakReference.get().getResources(), R.drawable.ic_menu_camera))
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .build();
